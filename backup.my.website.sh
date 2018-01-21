af2c1fa9bfc51519e2579acbe159ed2d43660f17
kashuorg <linzhibin824@gmail.com>
2018-01-21 08:17:58 +0800

    2018.01.20

--- backup.my.website.sh
+++ backup.my.website.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #Author: kashu
-#Date: 2014-01-24
-#Filename: backup.kashu.org.sh
-#Description: Backup my website: http://kashu.org
+#Date: 2018-01-21
+#Filename: website.backup.sh
+#Description: Backup my website and upload to dropbox.com
 
 BAK=/root/backup
 LOG=${BAK}/bak.log
 DATE=`date +%Y%m%d`
-if [ -s "/backup/${DATE}.tar.bz2" -a -s "/backup/${DATE}.sql.bz2" ]; then exit; fi
-tar -jcvpf ${BAK}/${DATE}.tar.bz2 /html/kashu.org/ &> /dev/null
-test -s ${BAK}/${DATE}.tar.bz2 || { echo "Error:${DATE}.tar.bz2" >> ${LOG} && exit 1; }
+if [ -s "/backup/${DATE}.tar.xz" -a -s "/backup/${DATE}.sql.xz" ]; then exit; fi
+tar -Jcvpf ${BAK}/${DATE}.tar.xz /var/www/html/kashu/ &> /dev/null
+test -s ${BAK}/${DATE}.tar.xz || { echo "Error:${DATE}.tar.xz" >> ${LOG} && exit 1; }
 
-mysqldump -uusername -ppassword DB_name > ${BAK}/${DATE}.sql
+mysqldump -uusername -ppassword database_name > ${BAK}/${DATE}.sql
 test -s ${BAK}/${DATE}.sql || { echo "Error:${DATE}.sql" >> ${LOG} && exit 2; }
-bzip2 -9 ${BAK}/${DATE}.sql
+xz -z9 ${BAK}/${DATE}.sql
 
-/root/shell/dropbox_uploader.sh upload ${BAK}/${DATE}*.bz2 /kashu.org
+/root/dropbox_uploader.sh upload ${BAK}/${DATE}*.xz /kashu
 if [ "$?" == 0 ]; then
 	echo "${DATE}:OK" >> ${LOG}
 else
